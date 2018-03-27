7265b7ef9fb7c924c38e58e4c8cd5292538eec36
nick <n.kravchenko@skins.services>
2018-03-27 18:33:23 +0300

    Starting to add OPSkins service

---
+++ src/services/opskins.js
@@ -0,0 +1,8 @@
+const OPSkinsAPI = require('@opskins/api');
+const opskins = new OPSkinsAPI('07f1dfdd1a18a79fb24f2d37e03016');
+
+class OPSkinsService{
+    constructor(){}
+}
+
+module.exports = new OPSkinsService();
